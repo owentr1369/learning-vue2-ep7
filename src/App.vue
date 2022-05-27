3d288dda427430125fe3574af18cc672b9812781
owentr1369 <tamvaa1306@gmail.com>
2022-05-27 16:19:20 +0700

    ep7 part1

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <h1>Hello</h1>
+    <div>{{ test }}</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      test: "Hello Vietnam",
+    };
+  },
+};
 </script>
 
 <style>
